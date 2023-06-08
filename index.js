41cec9ba71bc08d319e79198bacadc8bd0839b40
Mahfuz The Programmer Vetern <mahfuzzayn8@gmail.com>
2023-06-08 12:58:02 +0600

    Added Users POST Route

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send("Magic Minds Academy Server is Busy teaching magics...");
@@ -22,14 +27,28 @@ async function run() {
     try {
         await client.connect();
 
-        const usersCollection = client.db("magicMindsAcademyDB").collection('users');
+        const usersCollection = client
+            .db("magicMindsAcademyDB")
+            .collection("users");
 
-        app.post('/users', async (req, res) => {
-            const loggedUser = req.body;
-            const result = await usersCollection.insertOne(loggedUser);
+       
+
+        // Users API Routes
+        app.get('/users', async (req, res) => {
+            const result = await usersCollection.find().toArray();
             res.send(result);
-            
         })
+
+        app.post("/users", async (req, res) => {
+            const user = req.body;
+            const query = { email: user.email };
+            const existingUser = await usersCollection.findOne(query);
+            if (existingUser) {
+                return res.send({ message: "user already exists" });
+            }
+            const result = await usersCollection.insertOne(user);
+            res.send(result);
+        });
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
